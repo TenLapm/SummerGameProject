bc3780c4176b19878ac5536a09ae386c97259cb7
Ten <phakaphol.l@gmail.com>
2024-07-21 21:49:54 +0700

    WinPanel

--- Assets/Script/UI/InGameUI.cs
+++ Assets/Script/UI/InGameUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEditor.Animations;
 
 public class InGameUI : MonoBehaviour
 {
@@ -17,11 +18,15 @@ public class InGameUI : MonoBehaviour
     [SerializeField] private GameObject PlayerAWin;
     [SerializeField] private GameObject PlayerBWin;
 
+    private bool donefade;
+
+
     void Start()
     {
         Interface.SetActive(true);
         timer = GetComponent<Timer>();
         gameManager = FindObjectOfType<GameManager>();
+        donefade = false;
     }
 
     void Update()
@@ -35,7 +40,11 @@ public class InGameUI : MonoBehaviour
             Resume();
         }
 
-        if (timer.CurrentTime >= 60)
+        if (timer.CurrentTime < 0)
+        {
+            timerText.text = "0";
+        }
+        else if (timer.CurrentTime >= 60)
         {
             int minutes = Mathf.FloorToInt(timer.CurrentTime / 60);
             int seconds = Mathf.FloorToInt(timer.CurrentTime % 60);
@@ -44,9 +53,8 @@ public class InGameUI : MonoBehaviour
         else
         {
             timerText.text = string.Format("{0:0}", Mathf.FloorToInt(timer.CurrentTime));
-            
         }
-        if(gameManager.GameEnded)
+        if (gameManager.GameEnded)
         {
             GameEnd();
         }
@@ -75,11 +83,24 @@ public class InGameUI : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
     }
 
+    public void Restart()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MapSelect()
+    {
+        SceneManager.LoadScene("MapSelect");
+    }
+
     public void GameEnd()
     {
+        StartCoroutine(Transitioning(1));
+        if (donefade) { 
             PauseMenu.SetActive(false);
             Interface.SetActive(false);
-            Time.timeScale = 0.0f;
+            Time.timeScale = 1.0f;
 
             if (gameManager.playerAWin)
             {
@@ -89,7 +110,14 @@ public class InGameUI : MonoBehaviour
             {
                 PlayerBWin.SetActive(true);
             }
+        }
         
+    }
+
+    private IEnumerator Transitioning(int duration)
+    {
         
+        yield return new WaitForSeconds(duration);
+        donefade = true;
     }
 }
