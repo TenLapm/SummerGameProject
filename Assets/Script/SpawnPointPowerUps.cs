afe50e83d24b34e6dac438645eb4fed4ea8ddc49
nongpann <papran.tan@gmail.com>
2024-07-28 17:31:55 +0700

    Rate Power Up

--- Assets/Script/SpawnPointPowerUps.cs
+++ Assets/Script/SpawnPointPowerUps.cs
@@ -7,9 +7,11 @@ public class SpawnPointPowerUps : MonoBehaviour
     public int count = 0;
     public bool isMax = false;
     private float Timer = 0;
+    private int num = 0;
     [SerializeField] private float axisX = 0;
     [SerializeField] private float axisY = 0;
     [SerializeField] private List<GameObject> PowerUp = new List<GameObject>();
+    [SerializeField] private List<int> ratePowerUp = new List<int>();
     [SerializeField] private int limit = 5;
     [SerializeField] private float maxTimer = 5;
     [SerializeField] private float radius = 0.5f;
@@ -17,7 +19,9 @@ public class SpawnPointPowerUps : MonoBehaviour
 
     void Start()
     {
-        
+        for (int i = 0; i < ratePowerUp.Count; i++) { 
+            num += ratePowerUp[i];
+        }
     }
 
     
@@ -39,7 +43,8 @@ public class SpawnPointPowerUps : MonoBehaviour
     {
         if (count < limit && Timer <= 0)
         {
-            int random = Random.Range(0, PowerUp.Count - 1);
+            
+            int random = Random.Range(0, num);
             float randomX = Random.Range(-axisX / 2, axisX / 2);
             float randomY = Random.Range(-axisY / 2, axisY / 2);
             Vector2 pos = new Vector2(transform.position.x + randomX, transform.position.y + randomY);
@@ -47,11 +52,23 @@ public class SpawnPointPowerUps : MonoBehaviour
 
             if (collider == null)
             {
-                GameObject newChild = Instantiate(PowerUp[random], pos, Quaternion.identity);
-                newChild.transform.parent = transform;
-                isMax = false;
-                count++;
-                Timer = maxTimer;
+                int num2 = num;
+                int type = ratePowerUp.Count - 1;
+                for (int i = ratePowerUp.Count; i > 0; i--)
+                {
+                    if (random > num2 - ratePowerUp[i - 1])
+                    {
+                        GameObject newChild = Instantiate(PowerUp[type], pos, Quaternion.identity);
+                        newChild.transform.parent = transform;
+                        isMax = false;
+                        count++;
+                        Timer = maxTimer;
+                        break;
+                    }
+                    num2 -= ratePowerUp[i - 1];
+                    type--;
+                }
+                
             };
         }
     }
